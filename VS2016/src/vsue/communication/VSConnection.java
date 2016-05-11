b1dc1dd6382236682c9241e838523e407b97e1de
at <at@DESKTOP-P9PJBC3.fritz.box>
2016-05-11 16:51:48 +0200

    Initial commit

--- VS2016/src/vsue/communication/VSConnection.java
+++ VS2016/src/vsue/communication/VSConnection.java
@@ -1,6 +1,9 @@
 package vsue.communication;
 import java.io.IOException;
+import java.io.InputStream;
 import java.net.Socket;
+import java.nio.ByteBuffer;
+import java.nio.ByteOrder;
 
 public class VSConnection
 {
@@ -17,9 +20,17 @@ public class VSConnection
         _socket = socket;
     }
     
-
-    public void sendChunk(byte[]chunk) throws IOException // Todo
+    public void sendChunk(byte[]chunk) throws IOException
     {
+        int length = chunk.length;
+        
+        if (ByteOrder.nativeOrder().equals(ByteOrder.BIG_ENDIAN)) 
+            for(int i = 0; i < 4; i++)
+                _socket.getOutputStream().write((length >> i*8) & 0xFF);
+        else 
+            for(int i = 3; i <= 0; i--)
+                _socket.getOutputStream().write((length >> i*8) & 0xFF);
+         
         _socket.getOutputStream().write(chunk);
         
         _socket.getOutputStream().flush();
@@ -27,9 +38,48 @@ public class VSConnection
     
     public byte[] receiveChunk() throws IOException //Todo
     {
-        byte[] result = new byte[_socket.getInputStream().available()];
+        int length = 0;
+        
+        int start = 0;
+        int end  = 3;
+        int iter = 1;
+        
+        if(!ByteOrder.nativeOrder().equals(ByteOrder.BIG_ENDIAN))
+        {
+            start = 3;
+            end = 0;
+            iter = -1;
+        }
+        
+        for(int i = start; i <= end; i+=iter)
+        {
+            int b = _socket.getInputStream().read();
+            
+            // End of stream
+            if(b == -1)
+            {
+                throw new IOException();
+            }
                 
-        _socket.getInputStream().read(result);
+            length |= (b & 0xFF) << i*8;
+        }
+        
+        
+        byte[] result = new byte[length];
+                
+        int readBytes = 0;
+        
+        while(readBytes < length)
+        {
+            int bytes = _socket.getInputStream().read(result, readBytes, length - readBytes);
+            
+            if(bytes == 0)
+            {
+                throw new IOException();
+            }
+            
+            readBytes += bytes;
+        }
         
         return result;
     }
